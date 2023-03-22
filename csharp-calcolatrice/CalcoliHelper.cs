6f24c17ce17595531956c61aa34a9abb4c7168df
Luigi9411 <luigi.sciavicco@gmail.com>
2023-03-22 15:54:40 +0100

    Aggiungere i file di progetto.

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    public static class CalcoliHelper
+    {
+        public static double SumOfTwoIntegers(int num1, int num2) => num1 + num2;
+    }
+}
---
+++ csharp-calcolatrice/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using csharp_calcolatrice;
+
+//SOMMA INTERI
+Console.WriteLine("Inserisci il primo numero:");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci il secondo numero:");
+int num2 = int.Parse(Console.ReadLine());
+
+int sum = (int)CalcoliHelper.SumOfTwoIntegers(num1, num2);
+
+Console.WriteLine($"La somma tra {num1} e {num2} è {sum}");

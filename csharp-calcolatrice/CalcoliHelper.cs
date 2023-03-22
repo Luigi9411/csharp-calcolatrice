8f9416ae1991ccb4b226f55c02a4beb973b31184
Luigi9411 <luigi.sciavicco@gmail.com>
2023-03-22 17:52:03 +0100

    commit2

--- csharp-calcolatrice/CalcoliHelper.cs
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -4,10 +4,107 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace csharp_calcolatrice
 {
     public static class CalcoliHelper
     {
-        public static double SumOfTwoIntegers(int num1, int num2) => num1 + num2;
+        public static int SumOfTwoIntegers(int num1, int num2) => num1 + num2;
+        public static double SumOfTwoDoubleNumbers(double num3, double num4) => num3 + num4;
+        public static int DifferenceBetweenTwoIntegers(int num1, int num2) => num1 / num2;
+        public static double DifferenceBetweenTwoDoubleNumbers(double num3, double num4) => num3 / num4;
+        public static int MultiplicationOfTwoIntegers(int num1, int num2) => num1 * num2;
+        public static double MultiplicationOfTwoDoubleNumbers(double num3, double num4) => num3 * num4;
+        public static int AbsoluteValueOfAnInteger(int num1)
+        {
+            {
+                if (num1 < 0)
+                {
+                    return -num1;
+                }
+                else
+                {
+                    return num1;
+                }
+            }
+        }
+
+        public static double AbsoluteValueOfADoubleNumber(double num3)
+        {
+            {
+                if (num3 < 0)
+                {
+                    return -num3;
+                }
+                else
+                {
+                    return num3;
+                }
+            }
+        }
+
+        public static int LeastOfTwoIntegers(int num1, int num2)
+        {
+            {
+                if (num1 < num2)
+                {
+                    return num1;
+                }
+                else if (num2 < num1)
+                {
+                    return num2;
+                }
+                else num1 = num2;
+                {
+                    return num1;
+                }
+            }
+        }
+
+        public static double LeastOfTwoIntegers(double num3, double num4)
+        {
+            {
+                if (num3 < num4)
+                {
+                    return num3;
+                }
+                else if (num4 < num3)
+                {
+                    return num4;
+                }
+                else num3 = num4;
+                {
+                    return num3;
+                }
+            }
+        }
+
+        public static int MaximumOfTwoIntegers(int num1, int num2)
+        {
+            {
+                if (num1 < num2)
+                {
+                    return num2;
+                }
+                else
+                {
+                    return num1;
+                }
+            }
+        }
+
+        public static double MaximumOfTwoDoubleNumbers(double num3, double num4)
+        {
+            {
+                if (num3 < num4)
+                {
+                    return num4;
+                }
+                else
+                {
+                    return num3;
+                }
+            }
+        }
     }
 }
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -2,13 +2,46 @@
 using System;
 using csharp_calcolatrice;
 
-//SOMMA INTERI
+// INT
 Console.WriteLine("Inserisci il primo numero:");
 int num1 = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Inserisci il secondo numero:");
 int num2 = int.Parse(Console.ReadLine());
 
-int sum = (int)CalcoliHelper.SumOfTwoIntegers(num1, num2);
+int sum = CalcoliHelper.SumOfTwoIntegers(num1, num2);
+int dif = CalcoliHelper.DifferenceBetweenTwoIntegers(num1, num2);
+int mul = CalcoliHelper.MultiplicationOfTwoIntegers(num1, num2);
+int avi = CalcoliHelper.AbsoluteValueOfAnInteger(num1);
+int lea = CalcoliHelper.LeastOfTwoIntegers(num1, num2);
+int max = CalcoliHelper.MaximumOfTwoIntegers(num1, num2);
 
 Console.WriteLine($"La somma tra {num1} e {num2} è {sum}");
+Console.WriteLine($"La differenza tra {num1} e {num2} è {dif}");
+Console.WriteLine($"La moltiplicazione tra {num1} e {num2} è {mul}");
+Console.WriteLine($"Il valore assoluto di {num1} è {avi}");
+Console.WriteLine($"Il minimo tra due numeri interi {num1} e {num2} è {lea}");
+Console.WriteLine($"Il massimo tra due numeri interi {num1} e {num2} è {max}");
+
+// DOUBLE
+double num3 = 4.8;
+Console.WriteLine($"Il primo numero è {num3}");
+
+double num4 = 3.6;
+Console.WriteLine($"Il secondo numero è {num4}");
+
+double sum2 = CalcoliHelper.SumOfTwoDoubleNumbers(num3, num4);
+double dif2 = CalcoliHelper.DifferenceBetweenTwoDoubleNumbers(num3, num4);
+double mul2 = CalcoliHelper.MultiplicationOfTwoDoubleNumbers(num3, num4);
+double avi2 = CalcoliHelper.AbsoluteValueOfADoubleNumber(num3);
+double lea2 = CalcoliHelper.LeastOfTwoIntegers(num3, num4);
+double max2 = CalcoliHelper.MaximumOfTwoDoubleNumbers(num3, num4);
+
+Console.WriteLine($"La somma tra {num3} e {num4} è {sum2}");
+Console.WriteLine($"La differenza tra {num3} e {num4} è {dif2}");
+Console.WriteLine($"La moltiplicazione tra {num3} e {num4} è {mul2}");
+Console.WriteLine($"Il valore assoluto di {num3} è {avi2}");
+Console.WriteLine($"Il minimo tra due numeri double {num3} e {num4} è {lea2}");
+Console.WriteLine($"Il massimo tra due numeri double {num3} e {num4} è {max2}");
+Console.ReadLine();
+
